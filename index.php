3310e22d6db3c6546e344d9c6b3074c72965ff04
Lee Nugent <Lee@presencemultimedia.co.uk>
2011-01-24 13:39:38 +0000

    Added htaccess basic auth. Added ability to notify multiple clients.

--- index.php
+++ index.php
@@ -1,40 +1,51 @@
 <?php
-	
-	// The IP or Hostname of your mac
-	$ip = '192.168.3.50';
-	$password = 'astondb9';
-	
-	// Post or Pre deploy?
-	$hook = $_GET['hook'];
 
+	// Clients
+	$clients = array(
+		array('192.168.3.50', 'astondb9'),
+		array('leenug.webhop.org', 'astondb9')		
+	);
+
+	// Post or Pre deploy?
+	if(empty($_GET['hook'])) 
+	{
+		die('Hook not specified.'); 
+	}
+	else
+	{
+		$hook = $_GET['hook'];
+	}
+	
 	// decode the payload
 	$data = json_decode(file_get_contents('php://input'));
 		
 	// Growl Class
 	require_once('inc/class.growl.php'); 
 
-	// Setup
-	$growl = new Growl();
-	$growl->setAddress($ip, $password);
-	$growl->addNotification("Beanstalk-Deploy");
-	$growl->register();
-
-	// Send Notification
-	if($hook == "pre")
+	// Send
+	foreach($clients as $client)
 	{
-		$title = "'". ucfirst($data->repository) . "' deploying to ". ucfirst($data->environment) . " server.";
-		$message = '(Revision: ' . $data->revision . ") ";
-		$message .= $data->author . " - " . $data->comment;
-	
-		$growl->notify("Beanstalk-Deploy", $title, $message);
+		// Set Up
+		$growl = new Growl();
+		$growl->setAddress($client[0], $client[1]);
+		$growl->addNotification("Beanstalk-Deploy");
+		$growl->register();
+		
+		// Send Notification
+		if($hook == "pre")
+		{
+			$title = "'". ucfirst($data->repository) . "' deploying to ". ucfirst($data->environment) . " server.";
+			$message = '(Revision: ' . $data->revision . ") ";
+			$message .= $data->author . " - " . $data->comment;
+		
+			$growl->notify("Beanstalk-Deploy", $title, $message);
+		}
+		
+		if($hook == "post")
+		{
+			$title = "'". ucfirst($data->repository) . "' rev {$data->revision} was deployed to ". ucfirst($data->environment) . " server.";
+			$message = $data->author . " - " . $data->comment;
+		
+			$growl->notify("Beanstalk-Deploy", $title, $message);
+		}
 	}
-	
-	if($hook == "post")
-	{
-		$title = "'". ucfirst($data->repository) . "' rev {$data->revision} was deployed to ". ucfirst($data->environment) . " server.";
-		$message = $data->author . " - " . $data->comment;
-	
-		$growl->notify("Beanstalk-Deploy", $title, $message);
-	}
-	
-?>
